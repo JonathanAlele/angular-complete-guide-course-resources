62c4198d104ec6990999b377153b7b0b902b27c9
Jonathan Alele <jonathanalele@gmail.com>
2025-05-29 09:54:38 +0200

    Created the user input component

---
+++ attachments/04-essentials-practice/01-starting-project/01-starting-project/src/app/user-input/user-input.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  @Output() calculate = new EventEmitter<{ initialInvestment: number, annualInvestment: number, expectedReturn: number, duration: number }>();
+  enteredInitialInvestment: string = '0';
+  enteredAnnuallInvestment: string = '0';
+  enteredExpectedReturn: string = '5';
+  enteredDuration: string = '10';
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      annualInvestment: +this.enteredAnnuallInvestment,
+      expectedReturn: +this.enteredExpectedReturn,
+      duration: +this.enteredDuration
+    });
+  }
+}
